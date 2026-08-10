b555557279a9753b86cf19a247871afedc4fbb8b
Lightbeam-C <rhodescadence@gmail.com>
2026-08-11 09:27:19 +1200

    Added Type dropdown and Submit buttond

--- app/addquote/page.tsx
+++ app/addquote/page.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 const supabase = createClient('https://qyopevrsgdidmvnjhdxs.supabase.co', supabaseKey!)
+
 interface CustomDropdownProps {
   options: string[];
   selected: string;
@@ -18,20 +19,25 @@ export default function Home() {
     const router = useRouter()
     const pathname = usePathname()
     const whoPlaceholder = "Who"
-    const [text, setText] = useState("")
-    const [who, setWho] = useState("")
-    const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState('');
+    const typePlaceholder = "Type"
+    const [quoteParts, setQuoteParts] = useState([{ text:"", person:''}])
+    const [type, setType] = useState("")
+    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen2, setIsOpen2] = useState(false)
 
-    const people = ['All Categories', 'Inspiration', 'Humor', 'Philosophy'];
+    const people = ['Zico', 'Cadence', 'Griffin', 'James'];
+    const types = ['Sus', 'Non Sus', 'Misheard']
     
-    const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setText(e.target.value)
+    function handleTextareaChange(part:number, e: React.ChangeEvent<HTMLTextAreaElement>) {
+        const extraArray = [...quoteParts]
+        extraArray[part]['text'] = e.target.value
+        setQuoteParts(extraArray)
     
-        // 1. Reset height to 'auto' so it can shrink if text is deleted
         e.target.style.height = 'auto'
-        // 2. Set the height to match the internal content size
         e.target.style.height = `${e.target.scrollHeight}px`
+    }
+    async function addQuote(){
+
     }
 
     return (
@@ -43,24 +49,24 @@ export default function Home() {
                 <SidebarButton label="View Quotes" icon={<TextAlignStart size={20}/>} href='/'/>
             </div>
             <div className="flex flex-col items-center justify-center flex-1 bg-[#181A39] m-[10px] rounded-lg">
-                <div className="flex flex-col items-start w-full px-4">
+                <div className="flex flex-col items-start w-full px-4 space-y-1">
                     <h1 className='text-3xl'>Add Quotes</h1>
                     <div className="flex flex-row w-full">
                         <textarea 
-                            value = {text}
-                            onChange = {handleTextareaChange}
+                            value = {quoteParts[0]['text']}
+                            onChange = {(e) => handleTextareaChange(0, e)}
                             className='border-3 border-[#3A4358] bg-[#202531] rounded-lg w-full min-h-[37px] resize-none p-1 overflow-hidden mr-1' 
                             rows={1}
                             placeholder='Enter your quote...'
                         />
-                        <div className='w-40 h-[37px]'>
+                        <div className='w-60 h-full flex flex-row space-x-1 items-center'>
                             <div className="relative w-30">
                                 <button
                                     type="button"
                                     onClick={() => setIsOpen(!isOpen)}
                                     className="w-full flex justify-between items-center bg-blue-500 border border-slate-700 text-white px-4 py-2 rounded-lg text-sm focus:outline-none hover:border-slate-500 transition-colors"
                                 >
-                                    <span className="truncate">{selected || whoPlaceholder}</span>
+                                    <span className="truncate">{quoteParts[0]['person'] || whoPlaceholder}</span>
                                     <ChevronDown
                                     color='white'
                                     size={16}
@@ -76,10 +82,12 @@ export default function Home() {
                                         <button
                                             key={option}
                                             type="button"
-                                            onClick={() => {setSelected(option);
-                                            setIsOpen(false);
+                                            onClick={() => {const extraArray = [...quoteParts]
+                                                extraArray[0]['person'] = option
+                                                setQuoteParts(extraArray)
+                                                setIsOpen(false);
                                             }}
-                                            className="w-full text-left px-4 py-2 text-sm text-slate-300 hover:bg-blue-4    00 hover:text-white transition-colors"
+                                            className="w-full text-left px-4 py-2 text-sm text-slate-300 hover:bg-blue-400 hover:text-white transition-colors"
                                         >
                                             {option}
                                         </button>
@@ -87,6 +95,56 @@ export default function Home() {
                                     </div>
                                     </div>
                                 )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full">
+                        <div className="flex-1">
+                            
+                        </div>
+                        <div className="flex-1 flex flex-row justify-end space-x-1 mt-1">
+                            <div className="relative w-30">
+                                <button
+                                    type="button"
+                                    onClick={() => setIsOpen2(!isOpen2)}
+                                    className="w-full flex justify-between items-center bg-yellow-400 border border-slate-700 text-black px-4 py-2 rounded-lg text-sm focus:outline-none hover:border-slate-500 transition-colors"
+                                >
+                                    <span className="truncate">{type || typePlaceholder}</span>
+                                    <ChevronDown
+                                    color='black'
+                                    size={16}
+                                    className={`text-slate-400 transition-transform duration-200 ${
+                                        isOpen2 ? 'rotate-180' : ''
+                                    }`}
+                                    />
+                                </button>
+                                {isOpen2 && (
+                                    <div className="absolute top-full mt-1 w-full bg-yellow-400 border border-slate-700 rounded-lg shadow-xl overflow-hidden z-20">
+                                    <div className="max-h-60 overflow-y-auto">
+                                        {types.map((option) => (
+                                        <button
+                                            key={option}
+                                            type="button"
+                                            onClick={() => {setType(option);
+                                            setIsOpen2(false);
+                                            }}
+                                            className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-yellow-300 hover:text-black transition-colors"
+                                        >
+                                            {option}
+                                        </button>
+                                        ))}
+                                    </div>
+                                    </div>
+                                )}
+                            </div>
+                            <div className="relative w-30">
+                                <button
+                                    type="button"
+                                    onClick={(addQuote)}
+                                    className="w-full flex  justify-center items-center bg-green-500 border border-slate-700 text-black px-4 py-2 rounded-lg text-sm focus:outline-none hover:border-slate-500 hover:bg-green-400 transition-colors"
+                                >
+                                    <span className="truncate">Submit Quote</span>
+                                </button>
                             </div>
                         </div>
                     </div>
